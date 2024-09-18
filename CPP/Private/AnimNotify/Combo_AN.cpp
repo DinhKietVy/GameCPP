c0051883d36f51b2c8bc1bcc8df6d6aa3e6df075
DinhKietVy <116153273+DinhKietVy@users.noreply.github.com>
2024-09-18 20:44:53 +0700

    Delete CPP directory

--- CPP/Private/AnimNotify/Combo_AN.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "AnimNotify/Combo_AN.h"
-
-void UCombo_AN::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
-{
-	Super::Notify(MeshComp, Animation, EventReference);
-}
--- CPP/Public/Enum/CombatState.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// uint 8 -> so nguyen khong am 8 bit - 1 byte
-enum class ECombatState : uint8
-{
-	Ready,
-	Beaten
-};
